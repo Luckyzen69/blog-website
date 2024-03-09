a105b03081e67677854f827b58558db16fdb1103
Lagzen Thakuri <thakurizen2@gmail.com>
2024-03-09 15:35:05 +0545

    Update HTML, CSS, and JavaScript files for the blog website.

--- scripts/dataList.js
+++ scripts/dataList.js
@@ -8,8 +8,9 @@ function displayData(data) {
     const dataContainer = document.querySelector(".data-list");
 
     const htmlContent = data.map(item => `
-    <div class="deta" >
-    <div>
+     
+    <div class="deta"  >
+    <div onClick="detail(data)">
         <img src="${item.img}" alt="image">
         </div>
         <div>
@@ -18,6 +19,21 @@ function displayData(data) {
     </div>
     </div>
 `).join('');
-
+    
     dataContainer.innerHTML = htmlContent;
+}
+function detail(data){
+    const detailsContainer = document.getElementById('detailsContainer');
+    
+    // Clear previous content
+    detailsContainer.innerHTML = '';
+
+    // Create HTML content for each detail
+    for (const key in data) {
+        const detail = data[key];
+        const detailElement = document.createElement('p');
+        detailElement.textContent = `${key}: ${detail}`;
+        detailsContainer.appendChild(detailElement);
+    }   
+
 }
--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,8 @@ function displayData(data) {
     const dataContainer = document.querySelector(".recent");
 
     const htmlContent = data.map(item => `
-    <div class="data">
-        <img src="${item.img}" alt="">
+    <div class="data" ">
+        <img src="${item.img}" alt="image">
         <h2 class="item">${item.heading}</h2>
         <p class="item">${item.headline}</p>
     </div>
